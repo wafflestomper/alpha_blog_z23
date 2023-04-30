6255664012708fa19d73de3bddecfdb53af87faa
Brian Zollinhofer <brian.zollinhofer@gmail.com>
2023-04-30 16:32:08 -0400

    added new form and save to table

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,14 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to article_path(@article)
+  end
+
+
 end
